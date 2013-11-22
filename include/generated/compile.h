d6688b3f6f4450299db83e4605cf2a16740dfaa4
Hardslog <hardslog@gmail.com>
2013-11-22 17:32:05 -0500

    kexec hardboot patch, SELinux support for CM

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 19 */
+/* This file is auto generated, version 23 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#19 SMP PREEMPT Tue Nov 5 09:41:17 EST 2013"
+#define UTS_VERSION "#23 SMP PREEMPT Thu Nov 14 10:11:57 EST 2013"
 #define LINUX_COMPILE_BY "kmdavis"
 #define LINUX_COMPILE_HOST "GTNLinux01"
 #define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
