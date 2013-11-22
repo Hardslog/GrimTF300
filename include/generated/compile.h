8d81b6cce8fe33baa60221cd5ccdf23b81fa313a
Hardslog <hardslog@gmail.com>
2013-11-22 15:25:34 -0500

    init checkin and dynamic fsync

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 9 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 SMP PREEMPT Fri Oct 18 11:49:51 EDT 2013"
+#define LINUX_COMPILE_BY "kmdavis"
+#define LINUX_COMPILE_HOST "GTNLinux01"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
