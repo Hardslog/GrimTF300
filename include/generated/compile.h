15b003c476da20eb67096bb2da6caeeff9dda150
Hardslog <hardslog@gmail.com>
2013-11-22 16:47:18 -0500

    SmartassV2 and OC to 1.7

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Fri Oct 18 11:49:51 EDT 2013"
+#define UTS_VERSION "#12 SMP PREEMPT Mon Oct 21 13:28:39 EDT 2013"
 #define LINUX_COMPILE_BY "kmdavis"
 #define LINUX_COMPILE_HOST "GTNLinux01"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
+#define LINUX_COMPILER "gcc version 4.6 20120106 (prerelease) (GCC) "
