bf8fb3cc4c0661edbd0f4bae9e64ae010589b326
Hardslog <hardslog@gmail.com>
2013-11-22 17:18:42 -0500

    cleanup, fix dfsync, fix temp bootloop attempt

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 16 */
+/* This file is auto generated, version 19 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#16 SMP PREEMPT Wed Oct 23 16:37:37 EDT 2013"
+#define UTS_VERSION "#19 SMP PREEMPT Tue Nov 5 09:41:17 EST 2013"
 #define LINUX_COMPILE_BY "kmdavis"
 #define LINUX_COMPILE_HOST "GTNLinux01"
-#define LINUX_COMPILER "gcc version 4.6 20120106 (prerelease) (GCC) "
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
